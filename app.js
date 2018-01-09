83dc33db68e4f167dedaca0ee18ae170b15486d0
Jade Allen Cook <jcook@deseretnews.com>
2018-01-09 10:01:17 -0700

    🔗 Organized files to fix online demo

--- app.js
+++ app.js
@@ -8,4 +8,13 @@
     { title: 'Test 4', value: 13, color: 'red' }
   ];
   graphs.bar(barGraphOptions, barGraphData);
+  // create pie chart
+  var pieChartOptions = { elem: 'pie-chart', radius: 500 };
+  var pieChartData = [
+    { title: 'Test 1', value: 25, color: 'blue' },
+    { title: 'Test 2', value: 30, color: 'green' }, 
+    { title: 'Test 3', value: 23, color: 'purple' }, 
+    { title: 'Test 4', value: 13, color: 'red' }
+  ];
+  charts.pie(pieChartOptions, pieChartData);
 })();
